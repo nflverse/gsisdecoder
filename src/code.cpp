854da941e678722c3b1b00b1e9ef0c847bde15ac
= <=>
2021-06-28 20:04:55 -0500

    fix method ordering

--- src/code.cpp
+++ src/code.cpp
@@ -1,6 +1,27 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+std::string to_ascii(const std::string& cut) {
+  // convert to hex-string
+  std::string hex;
+
+  for (size_t k = 0; k < cut.size(); ++k) {
+    if (cut.substr(k, 1) != "-") {
+      hex += cut.substr(k, 1);
+    }
+  }
+
+  // get the ASCII representation
+  std::string ascii;
+  for (size_t i = 0; i < hex.size(); i += 2) {
+    const std::string part = hex.substr(i, 2);
+    char base16 = std::stoul(part, nullptr, 16);
+    ascii += base16;
+  }
+  return ascii;
+}
+
+
 // [[Rcpp::export]]
 CharacterVector decode_ids_cpp(const std::vector< std::string >& new_id){
   size_t n = new_id.size(); 
@@ -22,22 +43,3 @@ CharacterVector decode_ids_cpp(const std::vector< std::string >& new_id){
   return out;
 }
 
-std::string to_ascii(const std::string& cut) {
-  // convert to hex-string
-  std::string hex;
-
-  for (size_t k = 0; k < cut.size(); ++k) {
-    if (cut.substr(k, 1) != "-") {
-      hex += cut.substr(k, 1);
-    }
-  }
-
-  // get the ASCII representation
-  std::string ascii;
-  for (size_t i = 0; i < hex.size(); i += 2) {
-    const std::string part = hex.substr(i, 2);
-    char base16 = std::stoul(part, nullptr, 16);
-    ascii += base16;
-  }
-  return ascii;
-}
