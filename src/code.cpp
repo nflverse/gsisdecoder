3002066cf46794a52660be5fee9c4bb028aa1b29
= <=>
2021-06-28 20:04:09 -0500

    basic refactor of cpp code

--- src/code.cpp
+++ src/code.cpp
@@ -2,36 +2,42 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-CharacterVector decode_ids_cpp(std::vector< std::string > new_id){
-  int n = new_id.size();
+CharacterVector decode_ids_cpp(const std::vector< std::string >& new_id){
+  size_t n = new_id.size(); 
   CharacterVector out(n);
-  for(int j = 0; j < n; ++j) {
-    std::string id = new_id[j];
-    if (id.size() == 36){
-      std::string cut = id.substr(4, 24);
-      std::string hex = "";
-      for (unsigned int k = 0; k < cut.length(); ++k) {
-        if (cut.substr(k, 1) == "-") {
-          // hex = hex;
-        } else {
-          hex += cut.substr(k, 1);
-        }
-      }
-      std::string ascii = "";
-      for (size_t i = 0; i < hex.length(); i += 2){
-        //taking two characters from hex string
-        std::string part = hex.substr(i, 2);
-        //changing it into base 16
-        char ch = std::stoul(part, nullptr, 16);
-        //putting it into the ASCII string
-        ascii += ch;
-      }
-      out[j] = ascii;
-    } else if (id.length() > 5) {
-      out[j] = id;
-    } else {
-      out[j] = NA_STRING;
+
+  for (size_t j = 0; j < n; ++j) {
+    const std::string& id = new_id.at(j);
+    if (id.size() == 36) {
+      const std::string ascii = to_ascii(id.substr(4, 24));
+      out.at(j) = ascii;
+    }
+    else if (id.size() >= 5) {
+      out.at(j) = id;
+    }
+    else {
+      out.at(j) = NA_STRING;
     }
   }
   return out;
+}
+
+std::string to_ascii(const std::string& cut) {
+  // convert to hex-string
+  std::string hex;
+
+  for (size_t k = 0; k < cut.size(); ++k) {
+    if (cut.substr(k, 1) != "-") {
+      hex += cut.substr(k, 1);
+    }
+  }
+
+  // get the ASCII representation
+  std::string ascii;
+  for (size_t i = 0; i < hex.size(); i += 2) {
+    const std::string part = hex.substr(i, 2);
+    char base16 = std::stoul(part, nullptr, 16);
+    ascii += base16;
+  }
+  return ascii;
 }
